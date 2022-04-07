e0ca63ac8953be2ffaa4178a0024249fdbdd22e7
unknown <kelu33@gmail.com>
2022-04-07 11:51:28 +0200

    Update README.md

--- cypress/integration/list-not-book.spec.js
+++ cypress/integration/list-not-book.spec.js
@@ -3,10 +3,8 @@
 describe("Test non passant d'obtention d'une liste de livres", () => {
   it("test", () => {
     cy.bookSearch("badQuery").then((response) => {
-      expect(response.status).to.eql(200);
-      expect(response.body.totalItems).to.eql(0);
-      console.log(response.body)
-
-    });
-  });
-});
+      expect(response.status).to.eql(200)
+      expect(response.body.totalItems).to.eql(0)
+    })
+  })
+})
