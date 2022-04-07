ce9d7749b628b181a582ad5d41b47c05a7097851
auberon33 <51779120+auberon33@users.noreply.github.com>
2022-04-07 17:11:48 +0200

    commit loic 1

--- cypress/integration/list-book.spec.js
+++ cypress/integration/list-book.spec.js
@@ -16,12 +16,14 @@ const schema=
   }};
 describe(" tests", () => {
   it("test", () => {
-    cy.bookSearch("The way of Kings").then((response) => {
+    cy.bookSearch("The way of Kings", 3).then((response) => {
       cy.log(response.body);
       expect(response.status).to.eq(200)
       expect(response.body).to.not.be.null
       // expect(response.body).to.have.jsonSchema(schema);
+       cy.validateSchema(schema, response.body);
       expect(response.body.items[0].volumeInfo.title).to.deep.eq("The Way of Kings")
+      expect(response.body.items.length).to.deep.eq(3)
     });
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,11 @@
 /// <reference types="cypress" />
 
-Cypress.Commands.add("bookSearch", (query) => {
+Cypress.Commands.add("bookSearch", (query, limit) => {
   cy.request({
     url: "https://www.googleapis.com/books/v1/volumes",
     qs: {
       q: "intitle:" + query,
+      maxResults: limit=5
     },
   });
 });
